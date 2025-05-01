82a12290b737c9f3353a76e04ae4c2f715ff33e1
Davi Almeida <davi.almeida.135@ufrn.edu.br>
2025-05-01 16:57:34 -0300

    TODO tasks

--- officialproject/dags/transform.py
+++ officialproject/dags/transform.py
@@ -124,6 +124,150 @@ def sort_data(**context):
     context["task_instance"].xcom_push(key="tennis_df", value=df.to_json())
     df.to_csv(os.path.join(directory_path, 'data/sorted.csv'), index=False)
 
+
+def fix_categoric_data(**context):
+    import pandas as pd
+    print('Finalizing data...')
+    df_json = context["task_instance"].xcom_pull(task_ids="sort_data_task", key="tennis_df")
+    if not df_json:
+        raise ValueError("Could not retrieve weather_df from XCom.")
+    df = pd.read_json(df_json)
+
+    # Remove matches with walkover
+    print("Removing Walkovers")
+    df = df[df['score'] != 'W/O']
+    print("Removed Walkovers")
+
+    """Transform tournament level to categorical values"""
+    dictionary = {'D':0,'A':1,'M':2,'G':3,'F':4,}
+    df['tourney_level'] = df['tourney_level'].apply(lambda x: dictionary.get(x, 0))
+
+    """Transform handedness to categorical values"""
+    df['winner_hand'] = df['winner_hand'].apply(lambda x: 0 if x == 'R' else 1)
+    df['loser_hand'] = df['loser_hand'].apply(lambda x: 0 if x == 'R' else 1)
+
+def remove_stat_cols(**context):
+    """Remove unnecessary columns"""
+    import pandas as pd
+    print('Finalizing data...')
+    df_json = context["task_instance"].xcom_pull(task_ids="sort_data_task", key="tennis_df")
+    if not df_json:
+        raise ValueError("Could not retrieve weather_df from XCom.")
+    df = pd.read_json(df_json)
+
+    df = df.drop(columns=['w_ace',
+                     'w_df',
+                     'w_svpt',
+                     'w_1stIn',
+                     'w_1stWon',
+                     'w_2ndWon',
+                     'w_SvGms',
+                     'w_bpSaved',
+                     'w_bpFaced',
+                     'l_ace',
+                     'l_df',
+                     'l_svpt',
+                     'l_1stIn',
+                     'l_1stWon',
+                     'l_2ndWon',
+                     'l_SvGms',
+                     'l_bpSaved',
+                     'l_bpFaced',
+                     'score',
+                     'winner_ioc',
+                     'loser_ioc',
+                     'winner_id',
+                     'loser_id'
+                     ])
+    return df
+
+def anonymize(**context):
+    """
+    Troca os nomes dos jogadores por player1 e player2"
+    """
+    import pandas as pd
+    import numpy as np
+    print('Finalizing data...')
+    df_json = context["task_instance"].xcom_pull(task_ids="sort_data_task", key="tennis_df")
+    if not df_json:
+        raise ValueError("Could not retrieve weather_df from XCom.")
+    df = pd.read_json(df_json)
+
+    rows_list = []
+
+    # Iterar sobre as linhas do DataFrame trocando loser por player1 e winner por player2
+    # Fazer de maneira aleatória para evitar bias
+    for index, row in df.iterrows():
+        if np.random.randint(0, 2) == 0:
+            rows_list.append({
+                'tourney_id': row['tourney_id'],
+                'tourney_name': row['tourney_name'],
+                'surface': row['surface'],
+                'draw_size': row['draw_size'],
+                'tourney_level': row['tourney_level'],
+                'tourney_date': row['tourney_date'],
+                'match_num': row['match_num'],
+                'player1_id': row['loser_id'],
+                'player1_seed': row['loser_seed'],
+                'player1_entry': row['loser_entry'],
+                'player1_name': row['loser_name'],
+                'player1_hand': row['loser_hand'],
+                'player1_ht': row['loser_ht'],
+                'player1_ioc': row['loser_ioc'],
+                'player1_age': row['loser_age'],
+                'player1_rank': row['loser_rank'],
+                'player1_rank_points': row['loser_rank_points'],
+                'player0_id': row['winner_id'],
+                'player0_seed': row['winner_seed'],
+                'player0_entry': row['winner_entry'],
+                'player0_name': row['winner_name'],
+                'player0_hand': row['winner_hand'],
+                'player0_ht': row['winner_ht'],
+                'player0_ioc': row['winner_ioc'],
+                'player0_age': row['winner_age'],
+                'player0_rank': row['winner_rank'],
+                'player0_rank_points': row['winner_rank_points'],
+                'best_of': row['best_of'],
+                'round': row['round'],
+                'winner':0,
+            })
+        else:
+            rows_list.append({
+                'tourney_id': row['tourney_id'],
+                'tourney_name': row['tourney_name'],
+                'surface': row['surface'],
+                'draw_size': row['draw_size'],
+                'tourney_level': row['tourney_level'],
+                'tourney_date': row['tourney_date'],
+                'match_num': row['match_num'],
+                'player1_id': row['winner_id'],
+                'player1_seed': row['winner_seed'],
+                'player1_entry': row['winner_entry'],
+                'player1_name': row['winner_name'],
+                'player1_hand': row['winner_hand'],
+                'player1_ht': row['winner_ht'],
+                'player1_ioc': row['winner_ioc'],
+                'player1_age': row['winner_age'],
+                'player1_rank': row['winner_rank'],
+                'player1_rank_points': row['winner_rank_points'],
+                'player0_id': row['loser_id'],
+                'player0_seed': row['loser_seed'],
+                'player0_entry': row['loser_entry'],
+                'player0_name': row['loser_name'],
+                'player0_hand': row['loser_hand'],
+                'player0_ht': row['loser_ht'],
+                'player0_ioc': row['loser_ioc'],
+                'player0_age': row['loser_age'],
+                'player0_rank': row['loser_rank'],
+                'player0_rank_points': row['loser_rank_points'],
+                'best_of': row['best_of'],
+                'round': row['round'],
+                'winner':1,
+            })
+
+    anon_df = pd.DataFrame(rows_list)
+    return anon_df
+
 dag = DAG(
     'tennis_dag',
     schedule='0 23 * * *',
